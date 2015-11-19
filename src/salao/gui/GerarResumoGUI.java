7de4fbb61cb1be9ef39bc4e460023dc0fc46469d
Rubem Kalebe <rubemkalebe@hotmail.com>
2015-11-19 19:26:48 -0300

    Implementando resumo

--- src/salao/gui/GerarResumoGUI.java
+++ src/salao/gui/GerarResumoGUI.java
@@ -1,14 +1,24 @@
 package salao.gui;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 
 public class GerarResumoGUI extends AbstractGUI {
 
 	private static final long serialVersionUID = 1L;
+	
+	private JList<String> linhas;
+	
+	private DefaultListModel<String> listModel;
 
 	public GerarResumoGUI() {
 		super("Resumo das movimentações", 400, 350);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		linhas = new JList<String>();
+		listModel = new DefaultListModel<String>();
+		add(linhas);
+		listModel.addElement("teste");
 	}
 
 }
